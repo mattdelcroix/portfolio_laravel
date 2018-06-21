26b5add375f9cf994e420d4dbfda77e6eee00316
mattdelcroix <matt.delcroix@gmail.com>
2018-06-21 14:06:50 +0100

    Delete olds files, Fix links in the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,11 +2,12 @@
   <div class="row">
     <div id="div_brand" class="col-md-6 text-right">
       <span id="brand" class="align-middle">Matthieu Delcroix</span>
-    </div>
+    </div>    
     <div class="col-md-6 text-left">
-      <a target="_blank" title="follow me on facebook" href="http://www.facebook.com/matthieu.delcroix2"><i class="fab fa-facebook-square fa-3x"></i></a>
-      <a target="_blank" title="follow me on Twitter" href="http://www.twitter.com/PLACEHOLDER"><i class="fab fa-twitter-square fa-3x"></i></a>
-      <a target="_blank" title="follow me on linkedin" href="www.linkedin.com/in/matthieu-delcroix-14a7a7130"><i class="fab fa-linkedin fa-3x"></i></a>
+      <a target="_blank" title="Follow me on Facebook" href="http://www.facebook.com/matthieu.delcroix2"><i class="fab fa-facebook-square fa-3x"></i></a>
+      <a target="_blank" title="Follow me on Twitter" href="https://twitter.com/Matej77c"><i class="fab fa-twitter-square fa-3x"></i></a>
+      <a target="_blank" title="Follow me on Linkedin" href="https://www.linkedin.com/in/matthieu-delcroix-14a7a7130"><i class="fab fa-linkedin fa-3x"></i></a>
+       <a target="_blank" title="Visit my GitHub" href="https://github.com/mattdelcroix"><i class="fab fa-github-square fa-3x"></i></a>
     </div>
   </div>
 </footer>
